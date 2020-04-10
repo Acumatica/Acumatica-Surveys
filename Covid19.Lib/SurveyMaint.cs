22a7658f1cad10f40dfca541b89e47cd392a1728
Robert Waite <Rwaite@asifocus.com>
2020-04-09 20:35:32 -0400

    Checking in merger onto Robert Branch

--- Covid19.Lib/SurveyMaint.cs
+++ Covid19.Lib/SurveyMaint.cs
@@ -8,6 +8,13 @@ using PX.Objects.CR;
 
 namespace Covid19.Lib
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// Todo:   I suspect this is a redundant graph where SurveyQuizSettings seems to have the bulk of the work
+    ///         Im going to assume that we want to not use the word Quiz in the name.   
+    /// </remarks>
     public class SurveyMaint : PXGraph<SurveyMaint>
     {
         public SelectFrom<Surveys>.View SurveyList;
