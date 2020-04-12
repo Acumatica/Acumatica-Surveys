05ecc154ede1b46e13fabba80147dc3d429c717a
Robert Waite <Rwaite@asifocus.com>
2020-04-12 15:27:31 -0400

    Added Cleanup changes requested by Dhiren. Moved things so we will iterate only once and removed call to get mobile device.

--- PX.Survey.Ext/SurveyProcessV2.cs
+++ PX.Survey.Ext/SurveyProcessV2.cs
@@ -20,18 +20,9 @@ namespace PX.Survey.Ext
         //Fake for Design
         //public PXSelect<SurveyCollector> Records;
 
-        //todo: purge this view based on Collector records and replace with Recipients.
-        //      remove this dead code once the processing page is confirmed working.
-        /*
-        public PXFilteredProcessing<SurveyCollector, SurveyFilter,
-        Where<SurveyCollector.collectorStatus, Equal<SurveyResponseStatus.CollectorNewStatus>,
-                And<SurveyCollector.surveyID, Equal<Current<SurveyFilter.surveyID>>>>> Records;
-        */
-        
         public PXFilteredProcessing<SurveyUser, SurveyFilterV2,
             Where<SurveyUser.active, Equal<True>,
                 And<SurveyUser.surveyID, Equal<Current<SurveyFilterV2.surveyID>>>>> Records;
-
 
         public SurveyProcessV2()
         {
@@ -40,113 +31,87 @@ namespace PX.Survey.Ext
             Records.SetProcessDelegate(ProcessSurvey);
         }
 
-        //public static void ProcessSurvey(List<SurveyCollector> surveyList)
         public static void ProcessSurvey(List<SurveyUser> surveyUserList)
         {
-            bool erroroccurred = false;
-            List<SurveyCollector> collectorRecords = CreateCollectorRecords(surveyUserList);
-            SurveyCollectorMaint graph = PXGraph.CreateInstance<SurveyCollectorMaint>();
+            var errorOccurred  = false;
+            var graph          = PXGraph.CreateInstance<SurveyCollectorMaint>();
+            Survey surveyCurrent = null;
 
-            foreach (var rec in collectorRecords)
+            foreach (var surveyUser in surveyUserList)
             {
                 try
                 {
-                    graph.Clear();
-                    graph.SurveyQuestions.Current = graph.SurveyQuestions.Search<SurveyCollector.collectorID>(rec.CollectorID);
+                    //no need to search for the survey again if it was picked up on the previous round
+                    if (surveyCurrent == null || surveyCurrent.SurveyID != surveyUser.SurveyID)
+                    {
+                        //the primary need for this is to get the Survey Name to add it into the collector
+                        surveyCurrent =
+                            (Survey)PXSelect<Survey,
+                                    Where<Survey.surveyID, Equal<Required<Survey.surveyID>>>>
+                                .Select(graph, surveyUser.SurveyID).ToList().First();
+                    }
 
-                    if (graph.SurveyQuestions.Current.CollectorStatus != SurveyResponseStatus.CollectorNew) { continue; }
+                    var surveyCollector  = new SurveyCollector
+                    {
+                        CollectorName =
+                            $"{surveyCurrent.SurveyName} {PXTimeZoneInfo.Now:yyyy-MM-dd hh:mm:ss}",
+                        SurveyID = surveyUser.SurveyID,
+                        UserID = surveyUser.UserID,
+                        CollectedDate = null,
+                        ExpirationDate = null,
+                        CollectorStatus = "S" 
+                    };
+
+                    graph.Clear();
+                    graph.SurveyQuestions.Current = graph.SurveyQuestions.Search<SurveyCollector.collectorID>(surveyCollector.CollectorID);
+
+                    surveyCollector = graph.SurveyQuestions.Insert(surveyCollector);
+                    graph.Persist();
 
                     string sScreenID = PXSiteMap.Provider.FindSiteMapNodeByGraphType(typeof(SurveyCollectorMaint).FullName).ScreenID;
-                    Guid noteID = rec.NoteID.Value;
+                    
+                   
+                    Guid noteID = surveyCollector.NoteID.Value;
 
-                    PXTrace.WriteInformation("UserID " + rec.UserID.Value);
+                    PXTrace.WriteInformation("UserID " + surveyCollector.UserID.Value);
                     PXTrace.WriteInformation("noteID " + noteID.ToString());
                     PXTrace.WriteInformation("ScreenID " + sScreenID);
 
-                    MobileDevice device = PXSelectReadonly<MobileDevice,
-                                            Where<MobileDevice.userID, Equal<Required<MobileDevice.userID>>>>.Select(graph, rec.UserID.Value);
-                    if (device == null)
+                    if (surveyUser.UsingMobileApp != true)
                     {
                         throw new PXException(Messages.NoDeviceError);
                     }
 
                     var pushNotificationSender = ServiceLocator.Current.GetInstance<IPushNotificationSender>();
                     List<Guid> userIds = new List<Guid>();
-                    userIds.Add(rec.UserID.Value);
+                    userIds.Add(surveyCollector.UserID.Value);
 
                     pushNotificationSender.SendNotificationAsync(
                                         userIds: userIds,
                                         title: Messages.PushNotificationTitleSurvey,
-                                        text: $"{ Messages.PushNotificationMessageBodySurvey } # { rec.CollectorName }.",
+                                        text: $"{ Messages.PushNotificationMessageBodySurvey } # { surveyCollector.CollectorName }.",
                                         link: (sScreenID, noteID),
                                         cancellation: CancellationToken.None);
 
-                    graph.SurveyQuestions.Current.CollectorStatus = SurveyResponseStatus.CollectorSent;
-                    graph.SurveyQuestions.Update(graph.SurveyQuestions.Current);
-                    graph.Persist();
-
+                    
                     //todo: confirm with team if the bellow is correct.
-                    var userRecord = GetSurveyUserFromList(surveyUserList, rec);
-                    PXProcessing<SurveyUser>.SetInfo(surveyUserList.IndexOf(userRecord), Messages.SurveySent);
+                    PXProcessing<SurveyUser>.SetInfo(surveyUserList.IndexOf(surveyUser), Messages.SurveySent);
                 }
                 catch (AggregateException ex)
                 {
                     var message = string.Join(";", ex.InnerExceptions.Select(e => e.Message));
                     //todo: confirm with team if the bellow is correct.
-                    SurveyUser userRecord = GetSurveyUserFromList(surveyUserList, rec);
-                    PXProcessing<SurveyUser>.SetError(surveyUserList.IndexOf(userRecord), message);
+                    PXProcessing<SurveyUser>.SetError(surveyUserList.IndexOf(surveyUser), message);
                 }
                 catch (Exception e)
                 {
-                    erroroccurred = true;
+                    errorOccurred = true;
                     //todo: confirm with team if the bellow is correct.
-                    var userRecord = GetSurveyUserFromList(surveyUserList,rec);
-                    PXProcessing<SurveyUser>.SetError(surveyUserList.IndexOf(userRecord), e);
+                    PXProcessing<SurveyUser>.SetError(surveyUserList.IndexOf(surveyUser), e);
                 }
             }
-            if (erroroccurred)
+            if (errorOccurred)
                 throw new PXException(Messages.SurveyError);
-        }
-
-        private static SurveyUser GetSurveyUserFromList(List<SurveyUser> surveyUserList, SurveyCollector rec)
-        {
-            return surveyUserList.FirstOrDefault(x => x.UserID == rec.UserID);
-        }
-
-        private static List<SurveyCollector> CreateCollectorRecords(List<SurveyUser> surveyUserList)
-        {
-            List<SurveyCollector> collectorRecords = new List<SurveyCollector>();
-            var surveyMaint = PXGraph.CreateInstance<SurveyMaint>();
-
-            Survey surveyCurrent = null;
-
-            foreach (var user in surveyUserList)
-            {
-                //no need to search for the survey again if it was picked up on the previous round
-                if (surveyCurrent == null || surveyCurrent.SurveyID != user.SurveyID)
-                {
-                    surveyCurrent =
-                        (Survey)PXSelect<Survey,
-                                Where<Survey.surveyID, Equal<Required<Survey.surveyID>>>>
-                                .Select(surveyMaint, user.SurveyID).ToList().First();
-                }
-
-                var collector = new SurveyCollector
-                {
-                    CollectorName =
-                        $"{surveyCurrent.SurveyName} {PXTimeZoneInfo.Now.ToString("yyyy-MM-dd hh:mm:ss")}",
-                    SurveyID = user.SurveyID,
-                    UserID = user.UserID,
-                    CollectedDate = null,
-                    ExpirationDate = null,
-                    CollectorStatus = "N"
-                };
-                collector = surveyMaint.SurveyCollector.Insert(collector);
-                collectorRecords.Add(collector);
-            }
-
-            surveyMaint.Persist();
-            return collectorRecords;
         }
 
     }
@@ -175,57 +140,3 @@ namespace PX.Survey.Ext
     }
     #endregion
 }
-
-/* Temp area for aspx definition for reference only during development
-    todo: purge this once everything is confirmed working
-
-<%@ Page Language="C#" MasterPageFile="~/MasterPages/FormDetail.master" AutoEventWireup="true" ValidateRequest="false" CodeFile="SV501010.aspx.cs" Inherits="Page_SV501000" Title="Untitled Page" %>
-<%@ MasterType VirtualPath="~/MasterPages/FormDetail.master" %>
-
-<asp:Content ID="cont1" ContentPlaceHolderID="phDS" Runat="Server">
-    <px:PXDataSource ID="ds" runat="server" Visible="True" Width="100%" TypeName="PX.Survey.Ext.SurveyProcessV2" PrimaryView="Filter">
-	</px:PXDataSource>
-</asp:Content>
-<asp:Content ID="cont2" ContentPlaceHolderID="phF" Runat="Server">
-    <px:PXFormView ID="form" runat="server" DataSourceID="ds" Style="z-index: 100" DataMember="Filter" 
-		Width="100%" TabIndex="4500">
-		<Template>
-			<px:PXLayoutRule runat="server" StartRow="True" ControlSize="M" LabelsWidth="S"/>
-		    <px:PXSelector ID="edSurveyID" runat="server" CommitChanges="True" DataField="SurveyID" DisplayMode="Hint">
-            </px:PXSelector>
-		</Template>
-	</px:PXFormView>
-</asp:Content>
-<asp:Content ID="cont3" ContentPlaceHolderID="phG" Runat="Server">
-    <px:PXGrid ID="grid" runat="server" DataSourceID="ds" Style="z-index: 100" 
-		Width="100%" Height="150px" SkinID="Details" TabIndex="7400">
-		<Levels>
-			<px:PXGridLevel DataMember="Records" DataKeyNames="ContactID">
-			    <Columns>
-                    <%-- todo: purge this when confirmed working
-                        <px:PXGridColumn DataField="Selected" TextAlign="Center" Type="CheckBox" Width="60px" AllowCheckAll="true"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="SurveyID" TextAlign="Left" DisplayMode="Text" Width="100px"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="Userid" TextAlign="Left" DisplayMode="Text" Width="200px"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="CollectorID" TextAlign="Left" AllowMove="False" AllowResize="False" AllowShowHide="False" Width="0px"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="CollectorName" Width="250px"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="CollectedDate" Width="120px"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="ExpirationDate" Width="120px"></px:PXGridColumn>
-                    <px:PXGridColumn DataField="CollectorStatus" Width="150px"></px:PXGridColumn>--%>
-                    
-                    <px:PXGridColumn DataField="Selected" TextAlign="Center" Type="CheckBox" Width="60px" AllowCheckAll="true"></px:PXGridColumn>
-					<px:PXGridColumn DataField="Active" Type="CheckBox" TextAlign="Center" />
-                    <px:PXGridColumn DataField="ContactID" DisplayMode="Text"  TextAlign="Left" Width="250px" />
-                    <px:PXGridColumn DataField="RecipientType" />
-                    <px:PXGridColumn DataField="RecipientPhone" Width="180px" />
-                    <px:PXGridColumn DataField="RecipientEmail" Width="280px" />
-                    <px:PXGridColumn DataField="UsingMobileApp" TextAlign="Center" Type="CheckBox" Width="200px" />
-
-                </Columns>
-			</px:PXGridLevel>
-		</Levels>
-		<AutoSize Container="Window" Enabled="True" MinHeight="150" />
-	</px:PXGrid>
-</asp:Content>
-
-
- */
