16cdbfc52a2742ed5c7ac8fb0a7d95442fa1b3ea
Stéphane Bélanger <ste-bel@outlook.com>
2021-06-02 04:32:12 -0400

    Fix Collection graph.

--- PX.Survey.Ext/SurveyCollectorMaint.cs
+++ PX.Survey.Ext/SurveyCollectorMaint.cs
@@ -13,7 +13,7 @@ namespace PX.Survey.Ext {
         public SelectFrom<SurveyCollector>.View Collector;
         public SelectFrom<SurveyCollectorData>.Where<SurveyCollectorData.collectorID.IsEqual<SurveyCollector.collectorID.FromCurrent>>.View CollectedAnswers;
         public SelectFrom<SurveyCollectorData>.Where<SurveyCollectorData.surveyID.IsNull>.View UnprocessedCollectedAnswers;
-        public CRAttributeList<SurveyCollector> Answers;
+        //public CRAttributeList<SurveyCollector> Answers;
 
         public PXCancel<SurveyCollector> Cancel;
         public PXSave<SurveyCollector> Save;
@@ -24,33 +24,33 @@ namespace PX.Survey.Ext {
                 return;
             }
             bool bEnabled = (row.Status == CollectorStatus.Sent || row.Status == CollectorStatus.New);
-            Submit.SetEnabled(bEnabled);
-            Answers.Cache.AllowUpdate = (bEnabled);
+            //Submit.SetEnabled(bEnabled);
+            //Answers.Cache.AllowUpdate = (bEnabled);
             ReOpen.SetEnabled(row.Status == CollectorStatus.Responded);
-            PXUIFieldAttribute.SetDisplayName<CSAnswers.attributeID>(Answers.Cache, Messages.Question);
-            PXUIFieldAttribute.SetDisplayName<CSAnswers.value>(Answers.Cache, Messages.Answer);
+            //PXUIFieldAttribute.SetDisplayName<CSAnswers.attributeID>(Answers.Cache, Messages.Question);
+            //PXUIFieldAttribute.SetDisplayName<CSAnswers.value>(Answers.Cache, Messages.Answer);
         }
 
-        public PXAction<SurveyCollector> Submit;
-        [PXButton(CommitChanges = true)]
-        [PXUIField(DisplayName = Messages.Submit, MapViewRights = PXCacheRights.Select, MapEnableRights = PXCacheRights.Select)]
-        public virtual IEnumerable submit(PXAdapter adapter) {
-            Persist();
-            var currentQuestion = Collector.Current;
-            PXLongOperation.StartOperation(this, delegate () {
-                SurveyCollectorMaint graph = PXGraph.CreateInstance<SurveyCollectorMaint>();
-                graph.Collector.Current = graph.Collector.Search<SurveyCollector.collectorID>(currentQuestion.CollectorID);
-                if (graph.Answers.Select().ToList().Any(x => (x.GetItem<CSAnswers>().IsRequired.GetValueOrDefault(false) &&
-                                                             (String.IsNullOrEmpty(x.GetItem<CSAnswers>().Value))))) {
-                    throw new PXException(Messages.AnswerReqiredQuestions);
-                }
-                graph.Collector.Current.Status = CollectorStatus.Responded;
-                graph.Collector.Current.CollectedDate = PXTimeZoneInfo.Now;
-                graph.Collector.Update(graph.Collector.Current);
-                graph.Persist();
-            });
-            return adapter.Get();
-        }
+        //public PXAction<SurveyCollector> Submit;
+        //[PXButton(CommitChanges = true)]
+        //[PXUIField(DisplayName = Messages.Submit, MapViewRights = PXCacheRights.Select, MapEnableRights = PXCacheRights.Select)]
+        //public virtual IEnumerable submit(PXAdapter adapter) {
+        //    Persist();
+        //    var currentQuestion = Collector.Current;
+        //    PXLongOperation.StartOperation(this, delegate () {
+        //        SurveyCollectorMaint graph = PXGraph.CreateInstance<SurveyCollectorMaint>();
+        //        graph.Collector.Current = graph.Collector.Search<SurveyCollector.collectorID>(currentQuestion.CollectorID);
+        //        if (graph.Answers.Select().ToList().Any(x => (x.GetItem<CSAnswers>().IsRequired.GetValueOrDefault(false) &&
+        //                                                     (String.IsNullOrEmpty(x.GetItem<CSAnswers>().Value))))) {
+        //            throw new PXException(Messages.AnswerReqiredQuestions);
+        //        }
+        //        graph.Collector.Current.Status = CollectorStatus.Responded;
+        //        graph.Collector.Current.CollectedDate = PXTimeZoneInfo.Now;
+        //        graph.Collector.Update(graph.Collector.Current);
+        //        graph.Persist();
+        //    });
+        //    return adapter.Get();
+        //}
 
         public PXAction<SurveyCollector> ReOpen;
 
