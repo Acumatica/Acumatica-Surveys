827d9f5fc4bb20e7ab8c8b880dfbce004fbcef54
Stéphane Bélanger <ste-bel@outlook.com>
2021-06-02 14:05:39 -0400

    Not using the htmleditor anymore.

--- Pages/SU204003.aspx.cs
+++ Pages/SU204003.aspx.cs
@@ -12,22 +12,22 @@ public partial class Pages_SU_SU204003 : PX.Web.UI.PXPage
     protected void Page_Load(object sender, EventArgs e)
     {
     }
-	protected void edBody_BeforePreview(object src, PX.Web.UI.PXRichTextEdit.BeforePreviewArgs args)
-	{
-		var notification = (DefaultDataSource.DataGraph as SMNotificationMaint).Notifications.Current;
-		if (null != notification)
-		{
-			var info = PX.Api.ScreenUtils.ScreenInfo.TryGet(notification.ScreenID);
-			if (info != null)
-			{
-				args.GraphName = info.GraphName;
-				args.ViewName = info.PrimaryView;
-			}
-		}
-	}
-	protected void edBody_BeforeFieldPreview(object src, PX.Web.UI.PXRichTextEdit.BeforeFieldPreviewArgs args)
-	{
-		if (args.Type == typeof(PX.SM.Users) && args.FieldName == "UserList.Password")
-			args.Value = "*******";
-	}
+	//protected void edBody_BeforePreview(object src, PX.Web.UI.PXRichTextEdit.BeforePreviewArgs args)
+	//{
+	//	var notification = (DefaultDataSource.DataGraph as SMNotificationMaint).Notifications.Current;
+	//	if (null != notification)
+	//	{
+	//		var info = PX.Api.ScreenUtils.ScreenInfo.TryGet(notification.ScreenID);
+	//		if (info != null)
+	//		{
+	//			args.GraphName = info.GraphName;
+	//			args.ViewName = info.PrimaryView;
+	//		}
+	//	}
+	//}
+	//protected void edBody_BeforeFieldPreview(object src, PX.Web.UI.PXRichTextEdit.BeforeFieldPreviewArgs args)
+	//{
+	//	if (args.Type == typeof(PX.SM.Users) && args.FieldName == "UserList.Password")
+	//		args.Value = "*******";
+	//}
 }
