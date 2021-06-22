bdc21ea592378ce7f1b617b6e118fa419d69b7ee
Stéphane Bélanger <ste-bel@outlook.com>
2021-06-22 16:33:29 -0400

    Update statuses.

--- PX.Survey.Ext/Attributes/CollectorDataStatus.cs
+++ PX.Survey.Ext/Attributes/CollectorDataStatus.cs
@@ -5,20 +5,22 @@ namespace PX.Survey.Ext {
     public static class CollectorDataStatus {
         public class ListAttribute : PXStringListAttribute {
             public ListAttribute() : base(
-                new string[] { New, Connected, Processed, Error },
+                new string[] { New, Updated, Ignored, Processed, Error },
                 new string[] { 
-                    Messages.CollectorDataStatus.New, Messages.CollectorDataStatus.Connected,
+                    Messages.CollectorDataStatus.New, Messages.CollectorDataStatus.Updated, Messages.CollectorDataStatus.Ignored,
                     Messages.CollectorDataStatus.Processed, Messages.CollectorDataStatus.Error, 
                 }) { }
         }
 
         public const string New = "N";
-        public const string Connected = "C";
+        public const string Updated = "U";
+        public const string Ignored = "I";
         public const string Processed = "P";
         public const string Error = "E";
 
         public class _new : BqlString.Constant<_new> { public _new() : base(New) { } }
-        public class connected : BqlString.Constant<connected> { public connected() : base(Connected) { } }
+        public class updated : BqlString.Constant<updated> { public updated() : base(Updated) { } }
+        public class ignored : BqlString.Constant<ignored> { public ignored() : base(Ignored) { } }
         public class processed : BqlString.Constant<processed> { public processed() : base(Processed) { } }
         public class error : BqlString.Constant<error> { public error() : base(Error) { } }
     }
