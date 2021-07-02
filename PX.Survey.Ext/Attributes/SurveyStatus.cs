6e38319ca808b8380ce45fe0e3f6372bfb7a1170
Stéphane Bélanger <ste-bel@outlook.com>
2021-07-02 15:04:56 -0400

    New Status field on Survey

---
+++ PX.Survey.Ext/Attributes/SurveyStatus.cs
@@ -0,0 +1,26 @@
+﻿using PX.Data;
+using PX.Data.BQL;
+
+namespace PX.Survey.Ext {
+
+    public static class SurveyStatus {
+
+        public class ListAttribute : PXStringListAttribute {
+            public ListAttribute() : base(
+                new string[] { 
+                    Preparing, Started, 
+                    InProgress, Closed },
+                new string[] {
+                    Messages.SurveyStatus.Preparing, Messages.SurveyStatus.Started,
+                    Messages.SurveyStatus.InProgress, Messages.SurveyStatus.Closed,
+                }) { }
+        }
+
+        public const string Preparing = "P";
+        public const string Started = "S";
+        public const string InProgress = "W";
+        public const string Closed = "C";
+
+        public class closed : BqlString.Constant<closed> { public closed() : base(Closed) { } }
+    }
+}
