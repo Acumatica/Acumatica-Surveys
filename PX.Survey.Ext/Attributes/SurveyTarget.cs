25aa4bc414443e22e9ace2085adec9ae5944f2bf
Stéphane Bélanger <ste-bel@outlook.com>
2021-07-02 15:01:48 -0400

    Do not use Devices anymore

--- PX.Survey.Ext/Attributes/SurveyTarget.cs
+++ PX.Survey.Ext/Attributes/SurveyTarget.cs
@@ -5,17 +5,17 @@ namespace PX.Survey.Ext {
     public static class SurveyTarget {
         public class ListAttribute : PXStringListAttribute {
             public ListAttribute() : base(
-                new string[] { User, Contact, Anonymous, Device},
+                new string[] { User, Contact, Anonymous/*, Device*/},
                 new string[] { 
                     Messages.SurveyTarget.User, Messages.SurveyTarget.Contact,
-                    Messages.SurveyTarget.Anonymous, Messages.SurveyTarget.Device, 
+                    Messages.SurveyTarget.Anonymous/*, Messages.SurveyTarget.Device, */
                 }) { }
         }
 
         public const string User = "U";
         public const string Contact = "C";
         public const string Anonymous = "A";
-        public const string Device = "D";
+        //public const string Device = "D";
 
         //public class _new : BqlString.Constant<_new> { public _new() : base(New) { } }
         //public class connected : BqlString.Constant<connected> { public connected() : base(Connected) { } }
