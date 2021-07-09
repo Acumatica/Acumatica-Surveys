92237b193a3c8710b02624b3ed2828c41df83390
Stéphane Bélanger <ste-bel@outlook.com>
2021-07-09 02:05:06 -0400

    Added Entity Target.

--- PX.Survey.Ext/Attributes/SurveyTarget.cs
+++ PX.Survey.Ext/Attributes/SurveyTarget.cs
@@ -5,21 +5,21 @@ namespace PX.Survey.Ext {
     public static class SurveyTarget {
         public class ListAttribute : PXStringListAttribute {
             public ListAttribute() : base(
-                new string[] { User, Contact, Anonymous/*, Device*/},
+                new string[] { User, Contact, Anonymous, Entity },
                 new string[] { 
                     Messages.SurveyTarget.User, Messages.SurveyTarget.Contact,
-                    Messages.SurveyTarget.Anonymous/*, Messages.SurveyTarget.Device, */
+                    Messages.SurveyTarget.Anonymous, Messages.SurveyTarget.Entity,
                 }) { }
         }
 
         public const string User = "U";
         public const string Contact = "C";
         public const string Anonymous = "A";
-        //public const string Device = "D";
+        public const string Entity = "E";
 
         //public class _new : BqlString.Constant<_new> { public _new() : base(New) { } }
         //public class connected : BqlString.Constant<connected> { public connected() : base(Connected) { } }
         //public class processed : BqlString.Constant<processed> { public processed() : base(Processed) { } }
-        //public class error : BqlString.Constant<error> { public error() : base(Error) { } }
+        public class entity : BqlString.Constant<entity> { public entity() : base(Entity) { } }
     }
 }
