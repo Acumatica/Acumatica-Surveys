9b1e81171c43c86ed5f4d674292531e3e3b76279
Stéphane Bélanger <ste-bel@outlook.com>
2021-07-14 16:06:59 -0400

    Body should copy as well

--- PX.Survey.Ext/SurveyComponentMaint.cs
+++ PX.Survey.Ext/SurveyComponentMaint.cs
@@ -1,12 +1,10 @@
 ﻿using PX.Data;
-using System;
 
 namespace PX.Survey.Ext {
     public class SurveyComponentMaint : PXGraph<SurveyComponentMaint, SurveyComponent> {
 
         public PXSelect<SurveyComponent> SUComponent;
 
-        [PXCopyPasteHiddenFields(new Type[] { typeof(SurveyComponent.body) })]
         public PXSelect<SurveyComponent, Where<SurveyComponent.componentID, Equal<Current<SurveyComponent.componentID>>>> CurrentSUComponent;
 
         public SurveyComponentMaint() {
