0e457037ede90ffa484a7f30c222f584503182c4
Stéphane Bélanger <ste-bel@outlook.com>
2021-07-19 14:49:51 -0400

    Future code.

--- PX.Survey.Ext/DACExt/AbstractSurveyHandlerExt.cs
+++ PX.Survey.Ext/DACExt/AbstractSurveyHandlerExt.cs
@@ -2,6 +2,8 @@
 using PX.Data.DependencyInjection;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PX.Survey.Ext {
 
@@ -11,16 +13,18 @@ namespace PX.Survey.Ext {
 
         public PXSetup<SurveySetup> SurveySetup;
         public PXSelect<SurveySetupEntity, Where<SurveySetupEntity.entityType, Equal<Required<SurveySetupEntity.entityType>>>> SurveySetupEntity;
+        public PXSelect<SurveySetupEntity,
+            Where<SurveySetupEntity.entityType, Equal<Required<SurveySetupEntity.entityType>>,
+            And<SurveySetupEntity.surveyID, IsNotNull>>> SurveyIntegrations;
 
         public override void Initialize() {
             base.Initialize();
-            var setup = SurveySetup.Current;
             var primaryType = Base.PrimaryItemType;
             var entitySetup = GetEntitySetup(primaryType);
             var supportsSurvey = entitySetup != null && entitySetup.SurveyID != null;
-            //var showAction = b2Setup?.ShowTriggerActions == true;
             requestSurvey.SetEnabled(supportsSurvey); // TODO Keep enabled
-            //requestSurvey.SetVisible(supportsSurvey/* && showAction*/);
+            //var integrations = GetSurveyIntegrations(primaryType);
+            //AddIntegrations(integrations);
         }
 
         public PXAction<EDoc> requestSurvey;
@@ -51,5 +55,46 @@ namespace PX.Survey.Ext {
             var setup = SurveySetupEntity.SelectSingle(entityType.FullName);
             return setup;
         }
+
+        //private IEnumerable<SurveySetupEntity> GetSurveyIntegrations(Type entityType) {
+        //    var setups = SurveyIntegrations.Select(entityType.FullName).FirstTableItems;
+        //    return setups;
+        //}
+
+        //private void AddIntegrations(IEnumerable<SurveySetupEntity> integrations) {
+        //    if (!integrations.Any()) {
+        //        return;
+        //    }
+        //    foreach (SurveySetupEntity integration in integrations) {
+        //        var survey = Survey.PK.Find(Base, integration.SurveyID);
+        //        //AddAction(Base, survey);
+        //    }
+        //}
+
+        //private PXAction AddAction(PXGraph graph, Survey survey) {
+        //    var name = survey.SurveyID;
+        //    var displayName = survey.Title;
+        //    PXButtonDelegate handler = (PXAdapter adapter) => {
+        //        string str1 = (string.IsNullOrEmpty(adapter.CommandArguments) ? graph.PrimaryView : adapter.CommandArguments);
+        //        string primaryView = graph.PrimaryView;
+        //        PXCache cache = graph.Views[primaryView].Cache;
+        //        var current = (cache != null ? cache.Current : null);
+        //        return adapter.Get();
+        //    };
+        //    return AddAction(name, displayName, handler);
+        //}
+
+        //private PXAction AddAction(string name, string displayName, PXButtonDelegate handler) {
+        //    PXUIFieldAttribute pXUIFieldAttribute = new PXUIFieldAttribute() {
+        //        DisplayName = PXMessages.LocalizeNoPrefix(displayName),
+        //        MapEnableRights = PXCacheRights.Select
+        //    };
+        //    PXNamedAction<EDoc> pXNamedAction = new PXNamedAction<EDoc>(Base, name, handler, (new List<PXEventSubscriberAttribute>()
+        //    {
+        //        pXUIFieldAttribute
+        //    }).ToArray());
+        //    Base.Actions[name] = pXNamedAction;
+        //    return pXNamedAction;
+        //}
     }
 }
